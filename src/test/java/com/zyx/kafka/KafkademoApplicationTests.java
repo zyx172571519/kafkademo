a116bfad28ce8d4f738be753435006cc3dbefe2f
zyx172 <zyx172571@163.com>
2020-02-29 17:23:40 +0800

    Initial commit

---
+++ src/test/java/com/zyx/kafka/KafkademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyx.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkademoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
